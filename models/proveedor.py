c133bec5ae383262ff2bc769714bc813d1453a53
pocoy <pocoy@DESKTOP-65ERJ1P>
2021-01-11 13:17:42 +0100

    Agregado Vista Proveedores

--- models/proveedor.py
+++ models/proveedor.py
@@ -2,7 +2,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-#Lorena
+#Moroni
 
 from odoo import fields
 from odoo import models
@@ -15,7 +15,7 @@ class Proveedor(models.Model):
                              ])
     empresa = fields.Char(required=True)
     email = fields.Char(required=True)
-    mombre = fields.Char(required=True)
+    nombre = fields.Char(required=True)
     telefono = fields.Char(required=True)
     descripcion = fields.Char()
     
